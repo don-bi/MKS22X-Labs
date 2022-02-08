d864bc3f7450e286e3e175094237d84e8832f282
don-bi <dbi30@stuy.edu>
2022-02-08 14:43:52 -0500

    finished partialSum

--- classwork/2-7-22/challenges.java
+++ classwork/2-7-22/challenges.java
@@ -1,9 +1,9 @@
 public class challenges {
     public static void main(String[] args) {
-        
+        System.out.println(partialSum(new int[]{5,2,3,4,7}, 299));
     }
 
-    public static String toWords(int n){
+    /*public static String toWords(int n){
         String[] ones = {"zero","one","two","three","four","five",
                         "six","seven","eight","nine","ten"};
         String[] tens = {"eleven","twelve","thirteen","fourteen","fifteen","sixteen",
@@ -19,6 +19,21 @@ public class challenges {
         }
             else {
             return n%10 + sumDigits(n/10);
+        }
+    }*/
+
+    public static boolean partialSum(int[] arr,int targetValue){
+        return partialSum(0, arr, targetValue);
+    }
+
+    public static boolean partialSum(int start,int[] arr,int targetValue){
+        if (0 == targetValue){
+            return true;
+        } else if (start >= arr.length){
+            return false;
+        } else {
+            return partialSum(start+1, arr, targetValue-arr[start]) ||
+                partialSum(start+1, arr, targetValue);
         }
     }
 }
