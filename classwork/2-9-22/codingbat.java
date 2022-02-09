2d7cdc3ba207272805039f335017113c908cd9a7
don-bi <dbi30@stuy.edu>
2022-02-09 14:28:20 -0500

    RecursionClasswork finished splitArray i think

--- classwork/2-9-22/codingbat.java
+++ classwork/2-9-22/codingbat.java
@@ -1,6 +1,6 @@
 public class codingbat {
     public static void main(String[] args) {
-        
+        System.out.println(splitArray(new int[]{5,3,3}));
     }
 
     public static boolean groupSum(int start, int[] nums, int target){
@@ -9,8 +9,24 @@ public class codingbat {
         } else if (start >= nums.length){
             return false;
         } else {
-            return partialSum(start+1, nums, target-nums[start]) ||
-                partialSum(start+1, nums, target);
+            return groupSum(start+1, nums, target-nums[start]) ||
+                groupSum(start+1, nums, target);
+        }
+    }
+
+    public static boolean splitArray(int[] nums){
+        return splitArray(0, nums, 0);
+    }
+
+    public static boolean splitArray(int start, int[] nums, int value){
+        if (start == nums.length){
+            if (value == 0){
+                return true;
+            }
+            return false;
+        } else {
+            return splitArray(start+1, nums, value+nums[start]) ||
+                splitArray(start+1, nums, value-nums[start]);
         }
     }
 }
