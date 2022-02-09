c446f9106400a0ff272eb04b80de84e08f1b0ef6
don-bi <dbi30@stuy.edu>
2022-02-09 14:45:41 -0500

    RecursionClasswork more progress groupsum6

--- classwork/2-9-22/codingbat.java
+++ classwork/2-9-22/codingbat.java
@@ -33,12 +33,13 @@ public class codingbat {
 
     //groupsum6------
     public static boolean groupSum6(int start, int[] nums, int target){
-        if (nums[start] == 6) {
+        if (start >= nums.length){
+            if (0 == target) return true;
+            return false;
+        } else if (nums[start] == 6) {
             return groupSum6(start+1, nums, target-nums[start]);
         } else if (0 == target){
             return true;
-        } else if (start >= nums.length){
-            return false;
         } else {
                 return groupSum6(start+1, nums, target-nums[start]) ||
                 groupSum6(start+1, nums, target);
