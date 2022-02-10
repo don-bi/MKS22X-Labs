d060b9dd0906b957bb4dfa00cd60190e920632df
don-bi <dbi30@stuy.edu>
2022-02-10 14:49:55 -0500

    finished toWords for positive numbers (i think numbers that end 0 dont work yet though)

--- classwork/2-7-22/challenges.java
+++ classwork/2-7-22/challenges.java
@@ -1,26 +1,32 @@
 public class challenges {
     public static void main(String[] args) {
-        System.out.println(partialSum(new int[]{5,2,3,4,7}, 299));
+        System.out.println(toWords(76245));
     }
 
-    /*public static String toWords(int n){
-        String[] ones = {"zero","one","two","three","four","five",
+    public static String toWords(int n){
+        String[] ones = {"one","two","three","four","five",
                         "six","seven","eight","nine","ten"};
         String[] tens = {"eleven","twelve","thirteen","fourteen","fifteen","sixteen",
-                        "seventeen","eighteen","nineteen","twenty"};
+                        "seventeen","eighteen","nineteen"};
+        String[] tensbegin = {"twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"};
         if (n == 0){
-            return ones[n];
+            return "zero";
         } else if (n <= 10 && n > 0){
-            return ones[n];
-        } else if (n > 10 && n <= 20){
-            return tens[n - 10];
-        } else if (n > 20 && m < 30){
-            return "twenty" 
-        }
-            else {
-            return n%10 + sumDigits(n/10);
-        }
-    }*/
+            return ones[n - 1];
+        } else if (n > 10 && n < 20){
+            return tens[n%10-1];
+        } else if (n >= 20 && n < 100){
+            if (n%10==0){
+                return tensbegin[n/10-2];
+            } else {
+                return tensbegin[n/10-2] + "-" + toWords(n-n/10*10);
+            }
+        } else if (n >= 100 && n < 1000){
+            return ones[n/100-1] + " hundred " + toWords(n-n/100*100);
+        } else if (n >= 1000 && n < 1000000){
+            return toWords(n/1000) + " thousand " + toWords(n-n/1000*1000);
+        } return "";
+    }
 
     public static boolean partialSum(int[] arr,int targetValue){
         return partialSum(0, arr, targetValue);
