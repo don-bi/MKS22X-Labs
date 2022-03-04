0f3de2ba7e89354cbb18ad90098e5cc31a42b662
don-bi <dbi30@stuy.edu>
2022-03-04 14:23:40 -0500

    random direction method made

--- 03-maze/MazeGenerator.java
+++ 03-maze/MazeGenerator.java
@@ -1,12 +1,27 @@
+import java.util.ArrayList;
+
 public class MazeGenerator {
-    public static void generate(char[][]maze,int row, int col){
+    public static void generate(char[][] maze,int row, int col){
         if (maze[row][col] == '#' && checkConditions(maze, row, col)){
             maze[row][col] = ' ';
-            int[][] dir = {{row+1,col},{row-1,col},{row,col+1},{row,col-1}};
-            int randdir = (int)(Math.random()*4);
-            generate(randdir, row, col);
+            ArrayList<int[]> dir = makeDir(row, col);
+            for () 
+            generate(maze, row, col);
         } else {
         }
+    }
+    private static ArrayList<int[]> makeDir(int row, int col){
+        ArrayList<int[]> dir = new ArrayList<int[]>();
+        dir.add(new int[]{row+1,col});
+        dir.add(new int[]{row-1,col});
+        dir.add(new int[]{row,col+1});
+        dir.add(new int[]{row,col-1});
+        ArrayList<int[]> randdir = new ArrayList<int[]>();
+        for (int i = 4; i > -1; i --){
+            int rand = (int)(Math.random()*i);
+            randdir.add(dir.get(rand));
+        }
+        return randdir;
     }
 
     private static boolean checkConditions(char[][]m,int r,int c){
