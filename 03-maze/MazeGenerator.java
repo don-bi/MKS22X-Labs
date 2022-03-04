dcd0d92d446f82cc10fc344c0659e8436aef1d75
don-bi <dbi30@stuy.edu>
2022-03-04 14:12:14 -0500

    broken code before laptop died

--- 03-maze/MazeGenerator.java
+++ 03-maze/MazeGenerator.java
@@ -1,23 +1,22 @@
 public class MazeGenerator {
-    public static void generate(char[][]maze,int startrow, int startcol){
-        if (maze[row][col] != '#'){
-            
-        } else {
-            maze[row][col] = '@';
-            if(animate){
-                gotoTop();
-                System.out.println(this);
-                wait(50);
-            }
+    public static void generate(char[][]maze,int row, int col){
+        if (maze[row][col] == '#' && checkConditions(maze, row, col)){
+            maze[row][col] = ' ';
             int[][] dir = {{row+1,col},{row-1,col},{row,col+1},{row,col-1}};
-            for (int i = 0; i < dir.length; i ++){
-                int s = solve(dir[i][0],dir[i][1]);
-                if (s > -1) {
-                    return s + 1;
-                }
-            }
-            maze[row][col] = '.';
-            return -1;
+            int randdir = (int)(Math.random()*4);
+            generate(randdir, row, col);
+        } else {
         }
+    }
+
+    private static boolean checkConditions(char[][]m,int r,int c){
+        return m[r+1][c] != ' ' &&
+        m[r-1][c] != ' ' &&
+        m[r][c+1] != ' ' &&
+        m[r][c-1] != ' ' &&
+        r != 0 && 
+        r != m.length-1 && 
+        c != 0 && 
+        c != m[0].length-1; 
     }
 }
