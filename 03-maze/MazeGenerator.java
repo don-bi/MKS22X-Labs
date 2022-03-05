9ce4eca33e2a7cc959af8dbe98476ae869f86653
don-bi <dbi30@stuy.edu>
2022-03-05 18:33:18 -0500

    made add end and add start to generate

--- 03-maze/MazeGenerator.java
+++ 03-maze/MazeGenerator.java
@@ -8,17 +8,21 @@ public class MazeGenerator {
     }
 
     public static void generate(char[][] maze,int row,int col){
+        maze[row][col] = 'S';
         generate(maze, row, col, 0, 0, false);
+        addEnd(maze,row,col);
     }
 
     public static void generateAnimate(char[][] maze,int row, int col){
         clearTerminal();
         generate(maze, row, col, 0, 0, true);
     }
-    public static void generate(char[][] maze,int row, int col, int prevrow, int prevcol, boolean animate){
+    private static void generate(char[][] maze,int row, int col, int prevrow, int prevcol, boolean animate){
         if (maze[row][col] == '#' && isValid(maze, row, col, prevrow, prevcol)){
             //System.out.println("passes: [" + row + ", " + col + "]");
-            maze[row][col] = ' ';
+            if (maze[row][col] != 'S'){
+                maze[row][col] = ' ';
+            }
             if(animate){
                 gotoTop();
                 printMaze(maze);
@@ -26,11 +30,46 @@ public class MazeGenerator {
             }
             ArrayList<int[]> dir = makeDir(row, col);
             for (int i = 0; i < dir.size(); i ++){
-                generate(maze, dir.get(i)[0], dir.get(i)[1], row, col, true);
+                generate(maze, dir.get(i)[0], dir.get(i)[1], row, col, animate);
             } 
         }
     }
 
+    private static void addEnd(char[][] maze, int row, int col){
+        int[] furthest = {0,0};
+        int highestcount = 0;
+        for (int i = 0; i < maze.length; i ++){
+            for (int j = 0; j < maze[i].length; j ++){
+                if (maze[i][j] != '#'){
+                    maze[i][j] = 'E';
+                    if (solve(maze, row, col) > highestcount){
+                        furthest = new int[]{i,j};
+                    }
+                    maze[i][j] = ' ';
+                }
+            }
+        }
+        maze[furthest[0]][furthest[1]] = 'E';
+    }
+
+    private int solve(char[][] maze, int row, int col){
+        if (maze[row][col] != 'S' && maze[row][col] != ' '){
+            if (maze[row][col] == 'E') return 0;
+            return -1; 
+        } else {
+            maze[row][col] = '@';
+ 
+            int[][] dir = {{row+1,col},{row-1,col},{row,col+1},{row,col-1}};
+            for (int i = 0; i < dir.length; i ++){
+                int s = solve(maze, dir[i][0],dir[i][1]);
+                if (s > -1) {
+                    return s + 1;
+                }
+            }
+            maze[row][col] = '.';
+            return -1;
+        }
+    }
     //Method to randomly put the four directions in an array
     private static ArrayList<int[]> makeDir(int row, int col){
         ArrayList<int[]> dir = new ArrayList<int[]>();
@@ -48,7 +87,7 @@ public class MazeGenerator {
     }
 
     //checks if space is out of bounds or has less than two blanks around it
-    public static boolean isValid(char[][]m,int r,int c,int pr,int pc){
+    private static boolean isValid(char[][]m,int r,int c,int pr,int pc){
         boolean bool = true;
         bool = r != 0 && 
         r != m.length-1 && 
@@ -60,7 +99,7 @@ public class MazeGenerator {
                 for (int incc = -1; incc < 2; incc ++){
                     //System.out.println("check: [" + (r + incr) + ", " + (c + incc) + "]" + m[r + incr][c + incc]);
                     if (!(r + incr == pr && c + incc == pc)){
-                        if(m[r + incr][c + incc] == ' '){
+                        if(m[r + incr][c + incc] == ' ' || m[r +incr][c + incc] == 'S'){
                             countspace ++;
                         } 
                     }
@@ -98,7 +137,7 @@ public class MazeGenerator {
         System.out.println("\033[1;1H");
     }
 
-    public static void printMaze(char[][] maze){
+    private static void printMaze(char[][] maze){
         for (int i = 0; i < maze.length; i ++){
             for (int j = 0; j < maze[i].length; j ++){
                 System.out.print(maze[i][j]);
