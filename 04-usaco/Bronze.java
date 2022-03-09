63200137133c34a551700645e576ca975bd5c382
don-bi <dbi30@stuy.edu>
2022-03-09 13:41:06 -0500

    finished findhighest method and made most of stomp

--- 04-usaco/Bronze.java
+++ 04-usaco/Bronze.java
@@ -24,11 +24,35 @@ public class Bronze {
             int Rs = scan.nextInt();
             int Cs = scan.nextInt();
             int Ds = scan.nextInt();
-            
+
 
         } catch (FileNotFoundException e){
             System.out.println("lol");
         }
 
+    }
+
+    private static void stomp(int[][] field, int r, int c, int d){
+        int num = findHighest(field, r, c) - d;
+        for (int i = 0; i < 3; i ++){
+            for (int j = 0; j < 3; j ++){
+                if (field[r+i][j+c] > num){
+                    field[r+i][j+c] = num;
+                }
+            }
+        }
+    }
+
+    private static int findHighest(int f, int r, int c){
+        int highest = 0;
+        for (int i = 0; i < 3; i ++){
+            for (int j = 0; j < 3; j ++){
+                int current = field[r+i][j+c];
+                if (current > highest){
+                    highest = current;
+                }
+            }
+        }
+        return highest;
     }
 }
