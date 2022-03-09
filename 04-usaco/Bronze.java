fe405469a797f575c3a3cb0059a52b4ac7a5e8f7
don-bi <dbi30@stuy.edu>
2022-03-09 13:34:43 -0500

    actually finished scanning

--- 04-usaco/Bronze.java
+++ 04-usaco/Bronze.java
@@ -11,9 +11,21 @@ public class Bronze {
         try {
             File file = new File(filename);
             Scanner scan = new Scanner(file);
-            int[] field = new int[scan.nextInt()][scan.nextInt()];
+            int r = scan.nextInt();
+            int c = scan.nextInt();
+            int[][] field = new int[r][c];
             int elevation = scan.nextInt();
             int inputs = scan.nextInt();
+            for (int i=0; i < r; i++){
+                for (int j=0; j < c; j++){
+                    field[i][j] = scan.nextInt();
+                }
+            }
+            int Rs = scan.nextInt();
+            int Cs = scan.nextInt();
+            int Ds = scan.nextInt();
+            
+
         } catch (FileNotFoundException e){
             System.out.println("lol");
         }
