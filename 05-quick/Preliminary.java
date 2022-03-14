a87da6f2ed8b557c75316e95c8b53b7b81396780
don-bi <dbi30@stuy.edu>
2022-03-14 15:32:49 -0400

    progress on partition

--- 05-quick/Preliminary.java
+++ 05-quick/Preliminary.java
@@ -1,7 +1,26 @@
 public class Preliminary {
     
     public static int partition ( int [] data, int start, int end){
-        
+        int rand = (int)(Math.random()*(end-start+1))+start;
+        int temp = data[start];
+        data[start] = data[rand];
+        data[rand] = temp;
+        start++;
+        while (start != end) {
+            int P = data[start-1];
+            if (data[start] < P){
+                data[start-1] = data[start];
+                data[start] = P;
+                start++;
+            }
+            if (data[start] > P){
+                temp = data[start];
+                data[start] = data[end];
+                data[end] = temp;
+                end++;
+            }
+        }
+        return data[start];
     }
 
 }
