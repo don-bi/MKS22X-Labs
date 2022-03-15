1c4b6fb837858d36ccae7f676fad992d3a588a03
don-bi <dbi30@stuy.edu>
2022-03-15 14:51:16 -0400

    fixed partition

--- 05-quick/Preliminary.java
+++ 05-quick/Preliminary.java
@@ -2,9 +2,15 @@ import java.util.Arrays;
 
 public class Preliminary {
     public static void main(String[] args) {
-        int[] a = {999,999,999,4,3,2,1,0,5,6,3,4,3,2,999,999,999};
-        System.out.println(partition(a, 3, 13));
-        System.out.println(Arrays.toString(a));
+        /*int [] data = new int[] {997,998,999,4,3,2,1,0};
+        System.out.println("Original: "+Arrays.toString(data));
+        int pivot = partition( data , 3, 7);
+        System.out.println("Pivot value: "+data[pivot]+ ", Pivot index: "+pivot);
+        System.out.println("Modified: "+Arrays.toString(data));
+        System.out.println();*/
+        int[] ary = {2, 10, 15, 23, 0, 5};
+        //sorted(ary): {0, 2, 5, 10, 15, 23}
+        System.out.println(quickselect(ary, 1));
     }
     public static int partition ( int [] data, int start, int end){
         int rand = (int)(Math.random()*(end-start+1))+start;
@@ -16,7 +22,7 @@ public class Preliminary {
         int equaltimes = 0;
         //System.out.println(data[start]);
         start++;
-        while (start < end) {
+        while (start <= end) {
             //System.out.println("before: " + Arrays.toString(data));
             //System.out.println(P + " start: " + start + " end: " + end);
             if (data[start] < data[P] || (equaltimes % 2 == 0 && data[start] == data[P])){
@@ -36,10 +42,31 @@ public class Preliminary {
             //System.out.println("after: " + Arrays.toString(data));
             //System.out.println(P + " start: " + start + " end: " + end);
         }
-        temp = data[start];
-        data[start] = data[P];
+        temp = data[end];
+        data[end] = data[P];
         data[P] = temp;
         return end;
+    }
+
+
+    /*return the value that is the kth smallest value of the array.
+    *@param data must have a length > 0
+    *@param k is 0 to data.length-1 inclusive
+    *@postcondition The array may be modified. */
+    public static int quickselect(int []data, int k){
+        int start = 0;
+        int end = data.length-1;
+        int indexP = partition(data, start, end);
+        while (indexP != k){
+            if (indexP < k){
+                start = indexP+1;
+                indexP = partition(data, start, end);
+            } else if (indexP > k){
+                end = indexP-1;
+                indexP = partition(data, start, end);
+            }
+        }
+        return indexP;
     }
 
 }
