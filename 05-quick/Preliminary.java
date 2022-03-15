5e295ec9060593e47dd03f06d0bd0ed0f5cdfc18
don-bi <dbi30@stuy.edu>
2022-03-15 13:48:40 -0400

    finished speedy partition

--- 05-quick/Preliminary.java
+++ 05-quick/Preliminary.java
@@ -2,8 +2,8 @@ import java.util.Arrays;
 
 public class Preliminary {
     public static void main(String[] args) {
-        int[] a = {999,999,999,4,3,2,1,0,999,999,999};
-        System.out.println(partition(a, 3, 7));
+        int[] a = {999,999,999,4,3,2,1,0,5,6,3,4,3,2,999,999,999};
+        System.out.println(partition(a, 3, 13));
         System.out.println(Arrays.toString(a));
     }
     public static int partition ( int [] data, int start, int end){
@@ -12,17 +12,22 @@ public class Preliminary {
         int temp = data[start];
         data[start] = data[rand];
         data[rand] = temp;
+        int P = start;
+        int equaltimes = 0;
         //System.out.println(data[start]);
         start++;
-        while (start <= end) {
+        while (start < end) {
             //System.out.println("before: " + Arrays.toString(data));
-            int P = data[start-1];
             //System.out.println(P + " start: " + start + " end: " + end);
-            if (data[start] < P){
-                data[start-1] = data[start];
-                data[start] = P;
+            if (data[start] < data[P] || (equaltimes % 2 == 0 && data[start] == data[P])){
+                if (data[start] == data[P]){
+                    equaltimes ++;
+                }       
                 start++;
-            } else if (data[start] > P){
+            } else if (data[start] > data[P] || (equaltimes % 2 == 1 && data[start] == data[P])){
+                if (data[start] == data[P]){
+                    equaltimes ++;
+                }
                 temp = data[start];
                 data[start] = data[end];
                 data[end] = temp;
@@ -31,6 +36,9 @@ public class Preliminary {
             //System.out.println("after: " + Arrays.toString(data));
             //System.out.println(P + " start: " + start + " end: " + end);
         }
+        temp = data[start];
+        data[start] = data[P];
+        data[P] = temp;
         return end;
     }
 
