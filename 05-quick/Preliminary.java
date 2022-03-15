309308f48e81f0fdeb5e5aa58dea4b8e88f447fa
don-bi <dbi30@stuy.edu>
2022-03-14 21:21:14 -0400

    finished partition method

--- 05-quick/Preliminary.java
+++ 05-quick/Preliminary.java
@@ -1,26 +1,37 @@
+import java.util.Arrays;
+
 public class Preliminary {
-    
+    public static void main(String[] args) {
+        int[] a = {999,999,999,4,3,2,1,0,999,999,999};
+        System.out.println(partition(a, 3, 7));
+        System.out.println(Arrays.toString(a));
+    }
     public static int partition ( int [] data, int start, int end){
         int rand = (int)(Math.random()*(end-start+1))+start;
+        //System.out.println(rand);
         int temp = data[start];
         data[start] = data[rand];
         data[rand] = temp;
+        //System.out.println(data[start]);
         start++;
-        while (start != end) {
+        while (start <= end) {
+            //System.out.println("before: " + Arrays.toString(data));
             int P = data[start-1];
+            //System.out.println(P + " start: " + start + " end: " + end);
             if (data[start] < P){
                 data[start-1] = data[start];
                 data[start] = P;
                 start++;
-            }
-            if (data[start] > P){
+            } else if (data[start] > P){
                 temp = data[start];
                 data[start] = data[end];
                 data[end] = temp;
-                end++;
+                end--;
             }
+            //System.out.println("after: " + Arrays.toString(data));
+            //System.out.println(P + " start: " + start + " end: " + end);
         }
-        return data[start];
+        return end;
     }
 
 }
