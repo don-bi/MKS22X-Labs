ccc02eb65473555d1eb0704a98452909ef197522
don-bi <dbi30@stuy.edu>
2022-03-22 13:57:20 -0400

    fixed merge lol

--- 06-merge/Merge.java
+++ 06-merge/Merge.java
@@ -1,11 +1,24 @@
+import java.util.Arrays;
+
 public class Merge {
+    public static void main(String[] args) {
+        int[] one = {2,2,2,3,6,7};
+        int[] two = {1,2,3,5,8,10};
+        System.out.println(Arrays.toString(merge(one,two)));
+    }
     public static int [] merge(int [] left, int[] right){
         //return a new array that is the merged version of left and right
         int[] temp = new int[left.length + right.length];
         int lt = 0;
         int rt = 0;
         for (int i = 0; i < temp.length; i ++){
-            if (left[lt] < right[rt]){
+            if (rt >= right.length){
+                temp[i] = left[lt];
+                lt ++;
+            } else if (lt >= left.length){
+                temp[i] = right[rt];
+                rt ++;
+            } else if (left[lt] < right[rt]){
                 temp[i] = left[lt];
                 lt ++;
             } else if (left[lt] > right[rt]){
@@ -19,7 +32,7 @@ public class Merge {
         return temp;
       }
 
-      public static void mergesort(int [] data){
+      /*public static void mergesort(int [] data){
         int [] temp  =  mergeSortH(data);
         copy temp into data
       }
@@ -30,5 +43,5 @@ public class Merge {
           right = copy other half of data
           sort each half and merge them together
         }
-      }
+      }*/
 }
