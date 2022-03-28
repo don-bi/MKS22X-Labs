f2d4f5495735f5bd486608e0b0602de8dba0d7ad
don-bi <dbi30@stuy.edu>
2022-03-28 14:26:59 -0400

    constructors made

--- 07-mydeque/MyDeque.java
+++ 07-mydeque/MyDeque.java
@@ -3,8 +3,25 @@ public class MyDeque<E>{
     private int size;
     private int start, end;
 
-    public MyDeque(){  }
-    public MyDeque(int initialCapacity){  }
+    
+    public MyDeque(){
+        @SuppressWarnings("unchecked")
+        E[] d = (E[])new Object[10];
+        data = d;
+        start = data.length/2;
+        end = start + 1;
+        size = 0;
+    }
+
+    public MyDeque(int initialCapacity){  
+        @SuppressWarnings("unchecked")
+        E[] d = (E[])new Object[initialCapacity];
+        data = d;
+        start = data.length/2;
+        end = start + 1;
+        size = 0;
+    }
+
     public int size(){ }
     public String toString(){ }
     public void addFirst(E element){ }
@@ -13,4 +30,8 @@ public class MyDeque<E>{
     public E removeLast(){ }
     public E getFirst(){ }
     public E getLast(){ }
+
+    private void reisze(){
+
+    }
   }
