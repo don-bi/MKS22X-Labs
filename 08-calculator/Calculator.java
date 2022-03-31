ec23e4e696a999b072883050b7d993fe6c21ef3d
don-bi <dbi30@stuy.edu>
2022-03-31 10:11:32 -0400

    eval works

--- 08-calculator/Calculator.java
+++ 08-calculator/Calculator.java
@@ -1,4 +1,5 @@
 import java.util.ArrayDeque;
+import java.util.Scanner;
 
 public class Calculator {
     /*Evaluate a postfix expression stored in s.
@@ -9,5 +10,23 @@ public class Calculator {
       *        Use the string parameter of your exception to indicate what happened.
       *        Either "too many operands" or "too few operands for operation _" replace _ with +,- etc.
       */
-      public static double eval(String s){}
+    public static double eval(String s){
+        ArrayDeque<Double> stack = new ArrayDeque<Double>();
+        Scanner scan = new Scanner(s);
+        while (scan.hasNext()){
+            if (scan.hasNextDouble()){
+                stack.addFirst(scan.nextDouble());
+            } else {
+                double op2 = stack.removeFirst();
+                double op1 = stack.removeFirst();
+                char operator = scan.next().charAt(0);
+                if (operator == '+') stack.addFirst(op1 + op2);
+                if (operator == '-') stack.addFirst(op1 - op2);
+                if (operator == '/') stack.addFirst(op1 / op2);
+                if (operator == '*') stack.addFirst(op1 * op2);
+                if (operator == '%') stack.addFirst(op1 % op2);
+            }
+        }
+        return stack.getLast();
+    }
 }
